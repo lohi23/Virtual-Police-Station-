035109dba6ec08341c2b910c73a9316b70d0e55a
MORLA LOHITH <morlalohith@gmail.com>
2022-10-30 16:27:42 +0530

    commit

---
+++ admin/include/footer.php
@@ -0,0 +1,7 @@
+	<div class="footer">
+		<div class="container">
+
+
+			<b class="copyright"> Batch-65@VELTECH </b>
+		</div>
+	</div>
---
+++ users/includes/footer.php
@@ -0,0 +1,8 @@
+ <footer class="site-footer">
+          <div class="text-center">
+              &copy; BATCH-65 MINOR PROJECT @ VELTECH
+              <a href="#" class="go-top">
+                  <i class="fa fa-angle-up"></i>
+              </a>
+          </div>
+      </footer>
